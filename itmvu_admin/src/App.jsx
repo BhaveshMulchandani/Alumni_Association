5dbb8a4313ed00971bec448ffc49a1052bd0b0b1
BhaveshMulchandani <bhaveshmulchandani330@gmail.com>
2025-07-05 12:28:25 +0530

    complete job page and event page for admin

--- itmvu_admin/src/App.jsx
+++ itmvu_admin/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Verify_User from './pages/Verify_User'
+import Job from './pages/Job'
+import Event from './pages/Event'
 
 const App = () => {
   return (
@@ -9,7 +11,8 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/verify_user' element={<Verify_User/>}/>
-
+      <Route path='/job' element={<Job/>}/>
+      <Route path='/event' element={<Event/>}/>
     </Routes>
     </>
   )
